50bebe110a0481a858a8bb4ad5a64aefde0b0b25
qiangGe <qianggev5@126.com>
2019-06-29 18:44:57 +0800

    初始化

---
+++ Example/ZQViewController.h
@@ -0,0 +1,19 @@
+//
+//  ZQViewController.h
+//  Vc_Tests
+//
+//  Created by 志强 on 2019/6/29.
+//  Copyright © 2019 qiangGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZQViewController : UIViewController
+
+- (void) test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/ZQViewController.m
@@ -0,0 +1,36 @@
+//
+//  ZQViewController.m
+//  Vc_Tests
+//
+//  Created by 志强 on 2019/6/29.
+//  Copyright © 2019 qiangGe. All rights reserved.
+//
+
+#import "ZQViewController.h"
+
+@interface ZQViewController ()
+
+@end
+
+@implementation ZQViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)test{
+    NSLog(@"=======test");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
